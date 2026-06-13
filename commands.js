26f85f09ff64aceb20d227d95d3bbef05eac36b4
ankitsxn5143 <ankitsxn5143@gmail.com>
2026-06-13 14:45:25 +0530

    Update commands.js

--- commands.js
+++ commands.js
@@ -2,11 +2,15 @@ var FLAGGED_ADDRESSES = ["ankit@bwesglobal.com"];
 
 console.log("[RecipientWarning] commands.js loaded");
 
-var onItemSend = function(event) {
-  console.log("[RecipientWarning] onItemSend fired");
+Office.onReady(function() {
+  console.log("[RecipientWarning] Office.onReady called");
+});
+
+function onMessageSendHandler(event) {
+  console.log("[RecipientWarning] onMessageSendHandler fired!");
   var item = Office.context.mailbox.item;
+
   item.to.getAsync(function(result) {
-    console.log("[RecipientWarning] to.getAsync result:", result.status);
     var found = false;
     if (result.status === Office.AsyncResultStatus.Succeeded) {
       for (var i = 0; i < result.value.length; i++) {
@@ -18,20 +22,18 @@ var onItemSend = function(event) {
         }
       }
     }
-    console.log("[RecipientWarning] found flagged:", found);
+
     if (found) {
       item.notificationMessages.addAsync("warning", {
         type: Office.MailboxEnums.ItemNotificationMessageType.ErrorMessage,
         message: "Send blocked: flagged recipient detected."
-      }, function() { 
-        console.log("[RecipientWarning] send blocked");
-        event.completed({ allowEvent: false }); 
+      }, function() {
+        event.completed({ allowEvent: false });
       });
     } else {
-      console.log("[RecipientWarning] allowing send");
       event.completed({ allowEvent: true });
     }
   });
-};
+}
 
-console.log("[RecipientWarning] onItemSend registered:", typeof onItemSend);
+console.log("[RecipientWarning] onMessageSendHandler defined:", typeof onMessageSendHandler);
